f77ce14d66ccc86d4853d361d314bf046d5fb820
dhananshan <navin.dhananshan@outlook.com>
2019-04-05 00:11:49 +0800

    add command

--- src/extension.ts
+++ src/extension.ts
@@ -14,16 +14,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 
+	let disposableCommand = vscode.commands.registerCommand('extension.startgoogle', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Start Google!');
+	});
+
+
 	
-	let disposable = vscode.languages.registerCompletionItemProvider(['typescript', 'javascript'], {
+	let disposableCompletion = vscode.languages.registerCompletionItemProvider(['typescript', 'javascript'], {
 		provideCompletionItems(document: vscode.TextDocument, position, token, context: vscode.CompletionContext) {
+
+			const commandCompletion = new vscode.CompletionItem('google');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'google ';
+			commandCompletion.command = { command: 'extension.startgoogle', title: 'Re-trigger completions...' };
+
 		  return [
-			new vscode.CompletionItem('google', vscode.CompletionItemKind.Method)
+			commandCompletion
 		  ];
 		}
 	  },'.');
 
-	context.subscriptions.push(disposable);
+
+
+	context.subscriptions.push(disposableCommand);
+	context.subscriptions.push(disposableCompletion);
 }
 
 // this method is called when your extension is deactivated
