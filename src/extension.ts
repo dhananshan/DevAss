6a39fb13832375572fda7c5c0ed2774837669a2d
dhananshan <navin.dhananshan@outlook.com>
2019-04-06 23:42:09 +0800

    add user input, open package

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const open = require('open');
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,38 +12,67 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 		console.log('Activating devass!');
 
+		let dass = new DevAss();
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 
-	let disposableCommand = vscode.commands.registerCommand('extension.startgoogle', () => {
+	let disposableCommandDevAss = vscode.commands.registerCommand('extension.devass', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Start Google!');
+		dass.promptSearch();
 	});
 
-
+	let disposableCommandGoogle = vscode.commands.registerCommand('extension.google', () => {
+		// The code you place here will be executed every time your command is executed
+		dass.promptSearch();
+	});
 	
-	let disposableCompletion = vscode.languages.registerCompletionItemProvider(['typescript', 'javascript'], {
+	let disposableCompletion = vscode.languages.registerCompletionItemProvider([{ scheme: 'file', language: 'typescript' }], {
 		provideCompletionItems(document: vscode.TextDocument, position, token, context: vscode.CompletionContext) {
 
-			const commandCompletion = new vscode.CompletionItem('google');
-			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-			commandCompletion.insertText = 'google ';
-			commandCompletion.command = { command: 'extension.startgoogle', title: 'Re-trigger completions...' };
+			const commandCompletionGoogle = new vscode.CompletionItem('google');
+			commandCompletionGoogle.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletionGoogle.insertText = 'google ';
+			commandCompletionGoogle.command = { command: 'extension.devass', title: 'Developer Assistant' };
+
+			const commandCompletionDevAss = new vscode.CompletionItem('devAss');
+			commandCompletionDevAss.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletionDevAss.insertText = 'devAss ';
+			commandCompletionDevAss.command = { command: 'extension.devass', title: 'Developer Assistant' };
+
+			// remove type devass and google
+			//let  a = document.lineAt(position).text.substr(0, position.character);
 
 		  return [
-			commandCompletion
+				commandCompletionGoogle,
+				commandCompletionDevAss
 		  ];
 		}
 	  },'.');
 
 
 
-	context.subscriptions.push(disposableCommand);
+	context.subscriptions.push(disposableCommandDevAss);
+	context.subscriptions.push(disposableCommandGoogle);
 	context.subscriptions.push(disposableCompletion);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+class DevAss{
+
+	public async promptSearch(){
+		// Display a message box to the user
+		
+		let result = await vscode.window.showInputBox({
+			placeHolder: 'For example: best approach to sort an int array'
+		});
+		vscode.window.showInformationMessage(`Start Google! ${result}`);
+		open('https://www.google.com');
+
+	}
+
+}
